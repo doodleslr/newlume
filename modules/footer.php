2fae59a8621fe94763fea4922154b3c022a24784
Daniel <dglarocca@hotmail.com>
2019-03-21 15:29:59 +1100

    Adding PHP modularity

---
+++ modules/footer.php
@@ -0,0 +1,14 @@
+<footer>
+    <div class="nav-footer">
+        <ul>
+            <li id="current"><a href="#">Services</a></li>
+            <li id="current"><a href="#">Contact</a></li>
+        </ul>
+        <img src="img/bulb-logo.svg" alt="NewLume Light Bulb Logo" href="#home-header"/>
+        <ul>
+            <li id="current"><a href="#">About Us</a></li>
+            <li id="current"><a href="#">0406 603 080</a></li>
+        </ul>
+    </div>
+</footer>
+<p class="copyright"><i>&copy 2017-<?php echo date('Y'); ?> NewLume. All Rights Reserved.</i></p>
---
+++ modules/reviews.php
@@ -0,0 +1,9 @@
+<div class="customer-reviews">
+    <div id="customer-box">
+            <p>We believe that Sydney deserves a local electrician service that beyond reliable, at NewLume, we are committed to offering exactly that.
+                    We provide professional, personable tradesmen, exhibiting our trademark courtesy and a friendly and approachable demeanour.
+                    As local Sydney electricians for many years, we have tackled almost any job you can find for us! Our services are in demand across this city’s business and private landscapes, from full setups in professional offices, to simple appliance repair work.
+                    For a full list of our services, click here.</p>
+                    <h4>Dickbutt McTavish</h4>
+    </div>
+</div>
