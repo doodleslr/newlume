648243ad594dea72a27da55fd5270cfd1e0e82c8
Daniel <dglarocca@hotmail.com>
2019-03-25 15:52:28 +1100

    Add files via upload
    sql databases for services

---
+++ data/services.sql
@@ -0,0 +1,140 @@
+CREATE TABLE services (
+	id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,
+	icon VARCHAR NOT NULL,
+	title VARCHAR NOT NULL,
+	content VARCHAR NOT NULL,
+    alt VARCHAR
+);
+
+INSERT INTO
+    services 
+    (
+        icon, title, content, alt
+    )
+    VALUES (
+        'img/srv-construction.svg',
+        'New Builds',
+        'Building a new house? <br> Speak to us today.',
+        'A schematic icon for new house plans'
+    );
+INSERT INTO
+    services 
+    (
+        icon, title, content, alt
+    )
+    VALUES (
+        'img/srv-powerpoint.svg',
+        'Extra Power Points',
+        'Do you require more power outlets at home?',
+        'A power outlet icon'
+    );
+INSERT INTO
+    services 
+    (
+        icon, title, content, alt
+    )
+    VALUES (
+        'img/srv-rewire.svg',
+        'Home Rewiring',
+        'Old home wiring could prove unsafe.<br> Let us bring it up to standard.',
+        'A wire schematic icon'
+    );
+INSERT INTO
+    services 
+    (
+        icon, title, content, alt
+    )
+    VALUES (
+        'img/item-domestic.svg',
+        'Lighting',
+        'Enjoy your home inside and out with our LED and decorative lighting.',
+        'A lamp icon'
+    );
+INSERT INTO
+    services 
+    (
+        icon, title, content, alt
+    )
+    VALUES (
+        'img/srv-phone.svg',
+        'Phone Lines',
+        'Need a phone line or extra lines installed?',
+        'A telephone icon'
+    );
+INSERT INTO
+    services 
+    (
+        icon, title, content, alt
+    )
+    VALUES (
+        'img/srv-cctv.svg',
+        'CCTV Installation',
+        'Keep an eye on your home or business.',
+        'A CCTV camera icon'
+    );
+INSERT INTO
+    services 
+    (
+        icon, title, content, alt
+    )
+    VALUES (
+        'img/srv-fan.svg',
+        'Ceiling Fans',
+        'Keep cool and breezy with our ceiling fan installations.',
+        'A ceiling fan icon'
+    );
+INSERT INTO
+    services 
+    (
+        icon, title, content, alt
+    )
+    VALUES (
+        'img/srv-smoke.svg',
+        'Smoke Alarms',
+        'Keep safe and be proactive with early smoke detection.',
+        'A smoke alarm icon'
+    );
+INSERT INTO
+    services 
+    (
+        icon, title, content, alt
+    )
+    VALUES (
+        'img/item-solar.svg',
+        'Solar Power',
+        'For sustainability and power bill savings consider solar power.',
+        'A sun icon'
+    );
+INSERT INTO
+    services 
+    (
+        icon, title, content, alt
+    )
+    VALUES (
+        'img/item-switch.svg',
+        'Switchboard Upgrades',
+        'Outdated boards are prone to short-circuit, stay ahead with an upgrade.',
+        'A switchboard icon with buttons and dials'
+    );
+INSERT INTO
+    services 
+    (
+        icon, title, content, alt
+    )
+    VALUES (
+        'img/srv-fault.svg',
+        'Fault Finding',
+        'Problem solving and prevention with our fault finding service.',
+        'A magnifying glass icon'
+    );
+INSERT INTO
+    services 
+    (
+        icon, title, content, alt
+    )
+    VALUES (
+        'img/item-data.svg',
+        'Data Points',
+        'Install data points for home or office networks.',
+        'A USB data cable icon'
+    );
